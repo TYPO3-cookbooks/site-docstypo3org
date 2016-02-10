58ff5ea72f010c11d60028f07dd8b4551e9b0347
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-10 23:03:14 +0100

    Bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache2"
 description      "Additions to the setup of docs.typo3.org"
 long_description "Additions to the setup of docs.typo3.org"
-version          "1.0.0"
+version          "1.0.1"
 
 supports         "debian"
 
