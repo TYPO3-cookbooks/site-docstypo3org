4fbaeb707b61ae2e7d8ee59492780fed26084a51
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-10 23:02:56 +0100

    Include t3-base

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+include_recipe "t3-base"
+
 include_recipe "ssl_certificates"
 
 ssl_certificate node['site-docstypo3org']['ssl_certificate']
