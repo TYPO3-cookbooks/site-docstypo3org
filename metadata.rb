f089d7255d008dea64ec6418b38199b702b6ea81
Michael Stucki <michael@stucki.io>
2018-11-13 23:33:37 +0100

    [TASK] Update to Jenkins v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache2"
 description      "Additions to the setup of docs.typo3.org"
 long_description "Additions to the setup of docs.typo3.org"
-version          "1.0.3"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 supports         "debian"
 
